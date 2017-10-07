8ea64b958d2dce0e88ff19c4e8906b3ec9d5d2e2
rb <32129393+Malrb@users.noreply.github.com>
2017-10-07 05:17:25 -0500

    Create ICMP2Ping.py

---
+++ ICMP2Ping.py
@@ -0,0 +1,11 @@
+import os
+
+hostname = "localhost" #example
+response = os.system("ping -n 1 " + hostname)
+//If OS System - Linux , just use -c param
+
+#and then check the response...
+if response == 0:
+    print(hostname, 'is up!')
+else:
+    print(hostname, 'is down!')
