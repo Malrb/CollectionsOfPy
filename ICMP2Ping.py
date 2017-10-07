1c36db757b0d90520f1569dc714e4d8a1d65a6e1
rb <32129393+Malrb@users.noreply.github.com>
2017-10-07 05:17:57 -0500

    Update ICMP2Ping.py

--- ICMP2Ping.py
+++ ICMP2Ping.py
@@ -2,7 +2,7 @@ import os
 
 hostname = "localhost" #example
 response = os.system("ping -n 1 " + hostname)
-//If OS System - Linux , just use -c param
+#If OS System is Linux , just use -c param
 
 #and then check the response...
 if response == 0:
