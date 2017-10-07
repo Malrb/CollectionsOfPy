0191269873a12fe5f33a37c1ce57ecd230fa653f
rb <32129393+Malrb@users.noreply.github.com>
2017-10-07 09:37:57 -0500

    Update ICMP2Ping.py

--- ICMP2Ping.py
+++ ICMP2Ping.py
@@ -3,7 +3,7 @@ import os
 hostname = "localhost" #example
 response = os.system("ping -n 1 " + hostname)
 #If OS System is Linux , just use -c param
-
+# use -w (deadline) : 1 ,hold time will decrease
 #and then check the response...
 if response == 0:
     print(hostname, 'is up!')
